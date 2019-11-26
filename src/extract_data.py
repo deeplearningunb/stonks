2009aa2352fb4cb202779b47c9dc5c92a6c80c88
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-11-26 19:39:54 -0300

    Adding information

--- src/extract_data.py
+++ src/extract_data.py
@@ -4,10 +4,17 @@ from matplotlib import style
 import pandas_datareader.data as web
 
 style.use('ggplot')
+
+#date format: year/month/day 
 start = dt.datetime(1987,1,1)
 end = dt.datetime(2019,11,20)
+
+# To get another stock information
+# inform another stock name
 df = web.DataReader('TSLA', 'yahoo', start, end)
 
 df.plot()
 plt.show()
+
+#Remember to change the csv name
 df.to_csv('TSLA.csv')
