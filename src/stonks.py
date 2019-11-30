1d34abae85ebd40e37fc01fa51771fce4a630ca2
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-11-30 17:04:56 -0300

    Correcting code

--- src/stonks.py
+++ src/stonks.py
@@ -5,19 +5,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-base = pd.read('anyfile.csv')
+# pre processing data
+base = pd.read_csv('../Dataset/Training/TSLA.csv')
 base = base.dropna()
-base_training = base.iloc[:, 1:2].values]
-base_max_valor = base.iloc[:, 2:3].values]
+base_open_value = base.iloc[:, 3:4].values
+base_close_value = base.iloc[:, 4:5].values
 
 normalizer = MinMaxScaler(feature_range=(0,1))
-base_training_normalizer = normalizer.fit_transform(base_training)
-base_max_valor_normalizer = normalizer.fit_transform(base_max_valor)
-
-predictors = []
-real_price = []
-for i in range(90, 1242):
-    predictors.append(base_training_normalizer[i-90:i, 0])
-    real_price.append(base_training_normalizer[i, 0])
-predictors, real_price = np.array(predictors), np.array(real_price)
-predictors = np.reshape(predictors.sahpe[0], predictors.shape[1], 1)
+base_open_value_normalized = normalizer.fit_transform(base_open_value)
+base_close_value_normalized = normalizer.fit_transform(base_close_value)
