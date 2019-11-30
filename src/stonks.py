65df3a6e966a378311fc83435074940f927ec2c3
Danilow200 <danilow200@gmail.com>
2019-11-30 16:16:32 -0300

    Creating predictors and real price

--- src/stonks.py
+++ src/stonks.py
@@ -13,3 +13,11 @@ base_max_valor = base.iloc[:, 2:3].values]
 normalizer = MinMaxScaler(feature_range=(0,1))
 base_training_normalizer = normalizer.fit_transform(base_training)
 base_max_valor_normalizer = normalizer.fit_transform(base_max_valor)
+
+predictors = []
+real_price = []
+for i in range(90, 1242):
+    predictors.append(base_training_normalizer[i-90:i, 0])
+    real_price.append(base_training_normalizer[i, 0])
+predictors, real_price = np.array(predictors), np.array(real_price)
+predictors = np.reshape(predictors.sahpe[0], predictors.shape[1], 1)
